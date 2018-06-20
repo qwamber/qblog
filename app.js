16d88e9fa9001d171f2c40e40041ad2438e8ca1f
Marco Burstein <marco@marco.how>
2018-06-19 20:41:49 -0700

    Fix an ESLint error from a `console.log`.

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ let app = express();
 app.get('/', (req, res) => { res.send('Welcome to Qblog'); });
 
 let port = process.env.PORT || 8080;
+
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Qblog is listening on port ${port}.`));
