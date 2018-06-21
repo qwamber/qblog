aec135a892908d7aa33ef9100d3d84b584781e4a
Marco Burstein <marco@marco.how>
2018-06-21 09:02:42 -0700

    Add Less functionality through Webpack.

--- .webpack.config.js
+++ .webpack.config.js
@@ -12,6 +12,14 @@ module.exports = {
     module: {
         rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+            {
+                test: /\.less$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'less-loader' }
+                ],
+            },
         ],
     },
 };
